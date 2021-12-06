56416adf60aab720d30a182c53935efe96b17155
seppedewitte <seppe.dewitt@gmail.com>
2021-12-06 11:57:16 +0100

    push

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>my cool PWA App</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <button v-if="updateAvailable">click to update app</button>
+    <button v-if="updateAvailable" @click="update">click to update app</button>
     <p> Created by Seppe De Witte </p>
   </div>
 </template>
