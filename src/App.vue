a409a525b4d7677a16f47c7aeba9280e1c7828fc
seppedewitte <seppe.dewitt@gmail.com>
2021-12-06 12:18:19 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,47 @@
 <template>
   <div id="app">
-    <h1>my cool PWA App</h1>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <button v-if="updateAvailable" @click="update">click to update app</button>
-    <p> Created by Seppe De Witte </p>
+    <h1> This is my first ever PWA Application </h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <button  v-if="updateAvailable" @click="update">Click to update application</button>
+    <p> This site is created by Seppe De Witte for the Mobile Apps course</p>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "App",
-  components: {},
+  name: 'App',
+  components: {
+  },
   created() {
-    document.addEventListener("swupdatefound", this.updateTheApp, {
-      once: true,
-    });
+    document.addEventListener(
+      'swupdatefound', this.updateTheApp, { once: true }
+    );
+    navigator.serviceWorker.addEventListener( 'controllerchange', () => {
+      if (this.isRefreshing) return; 
+      this.isRefreshing =  true; 
+      window.location.reload();
+      }
+    ); 
   },
-  data: function () {
-    return {
-      registration: null,
-      updateAvailable: false,
-    };
-  },
+  data: () => ({
+    registration: null,
+    updateAvailable: false,
+    isRefreshing: false
+  }),
   methods: {
     updateTheApp(e) {
-      this.registration = e.detail;
+      this.registration = e.detail
       this.updateAvailable = true;
     },
-    update() {
-      this.updateAvailable = false;
-      if (this.registration || this.registration.waiting) {
-        this.registration.waiting.postMessage({ type: "SKIP_WAITING" });
+    update(){ 
+      this.updateAvailable =  false; 
+      if (this.registration || this.registration.waiting){ 
+        this.registration.waiting.postMessage({type:'SKIP_WAITING'});   
       }
     },
-  },
-};
+  }
+}
 </script>
 
 <style>
