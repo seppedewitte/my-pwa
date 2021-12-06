37a14d8dace5afbdf78151f6c4dac293f316a23c
seppedewitte <seppe.dewitt@gmail.com>
2021-12-06 11:51:35 +0100

    robin heeft geluk

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <template>
   <div id="app">
-    <h1> my cool PWA App </h1>
+    <h1>my cool PWA App</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
     <button v-if="updateAvailable">click to update app</button>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
-  components: {
-    
-  },
+  components: {},
   created() {
     document.addEventListener("swupdatefound", this.updateTheApp, {
       once: true,
     });
   },
-  data: function() {
-    return{
+  data: function () {
+    return {
       registration: null,
-      updateAvailable: false
-    }
+      updateAvailable: false,
+    };
   },
   methods: {
-    updateTheApp(e){
-      this.registration = e.detail
+    updateTheApp(e) {
+      this.registration = e.detail;
       this.updateAvailable = true;
-
-    }
-  }
+    },
+    update() {
+      this.updateAvailable = false;
+      if (this.registration || this.registration.waiting) {
+        this.registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      }
+    },
+  },
 };
 </script>
 
---
+++ src/service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('message', (event) => {
+    console.log(event)
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+    }
+});
+   
